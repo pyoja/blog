fc7ec985beb559cfe99b68b7fab91b473c2a2700
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-26 16:53:47 +0900

    컨트롤러 및 서버측 코드 작성

---
+++ blog/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using blog.Data;
+using blog.Models;
+
+namespace blog.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Register
+        [HttpPost]
+        public JsonResult Register(User model)
+        {
+            // 회원 가입 처리 코드 작성
+            using (var context = new ApplicationDbContext())
+            {
+                context.Users.Add(new User
+                {
+                    Name = model.Name,
+                    Id = model.Id,
+                    Password = model.Password,
+                    RegDate = DateTime.Now,
+                    DelState = 0
+                });
+
+                context.SaveChanges();
+            }
+
+            return Json(new { result = "success" });
+        }
+
+        // GET: All Users
+        public IEnumerable<User> GetAllUsers()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.Users.ToList();
+            }
+        }
+    }
+}
--- blog/Controllers/HomeController.cs
+++ blog/Controllers/HomeController.cs
@@ -26,5 +26,7 @@ namespace blog.Controllers
 
             return View();
         }
+
+
     }
 }
