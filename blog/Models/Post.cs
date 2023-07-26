67e1c393b908fac6b2704a46e36d96cc4ee1c8ad
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-26 14:54:53 +0900

    Models 데이터 모델 생성(User, Post)

---
+++ blog/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blog.Models
+{
+    public class Post
+    {
+        public int No { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImgUrl { get; set; }
+        public int Like { get; set; }
+        public string Comment { get; set; }
+        public DateTime RegDate { get; set; }
+        public byte DelState { get; set; }
+    }
+}
---
+++ blog/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace blog.Models
+{
+    public class User
+    {
+        public int No { get; set; }
+        public string Id { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public DateTime RegDate { get; set; }
+        public byte DelState { get; set; }
+    }
+
+}
