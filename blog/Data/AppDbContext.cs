f4eeb9401225a59071f4eac17f5cc5d04462afa4
pyoja <113084653+pyoja@users.noreply.github.com>
2023-07-27 14:16:16 +0900

    [로그인] 3. 데이터베이스 컨텍스트 설정하기

--- blog/Data/AppDbContext.cs
+++ blog/Data/AppDbContext.cs
@@ -1,18 +1,23 @@
-﻿// Data/ApplicationDbContext.cs
-
+﻿using blog.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System.Data.Entity;
-using blog.Models;
 
 namespace blog.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         // 기본 생성자 추가
         public AppDbContext() : base("name=_db_Blog")
         {
         }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
+
+        // 필요한 경우 추가 구성 코드
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // 추가 구성 코드 작성
+        }
     }
 }
